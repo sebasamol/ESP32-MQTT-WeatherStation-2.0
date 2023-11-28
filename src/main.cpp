8306ab42be04f98b6b6392c92c9c5e52d3a1bd27
sebasamol <sebasamol@gmail.com>
2023-11-28 18:41:41 +0100

    Connect to WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,30 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+
+const char* ssid = "INEA-0082_2.4G";
+const char* pwd = "4YC3Hbce";
+
 
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(9600);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, pwd);
+    Serial.println("\nConnecting");
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -13,6 +32,3 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
